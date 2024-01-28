c49137d64ba6e98bb27f7a81f55eaba6cc1dfafe
SARTHAK PANDEY <123884857+sar123-bot@users.noreply.github.com>
2024-01-28 11:44:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ document.addEventListener("keydown", function (Event) {
 function makesound(key) {
   switch (key) {
     case "w":
-      var crash = new Audio("./sounds/crash.mp3");
+      var crash = new Audio("./crash.mp3");
       crash.play();
       break;
     case "a":
-      var kick = new Audio("./sounds/kick-bass.mp3");
+      var kick = new Audio("./kick-bass.mp3");
       kick.play();
       break;
     case "s":
-      var snare = new Audio("./sounds/snare.mp3");
+      var snare = new Audio("./snare.mp3");
       snare.play();
       break;
     case "d":
-      var tom_1 = new Audio("./sounds/tom-1.mp3");
+      var tom_1 = new Audio("./tom-1.mp3");
       tom_1.play();
       break;
     case "j":
-      var tom_2 = new Audio("./sounds/tom-2.mp3");
+      var tom_2 = new Audio("./tom-2.mp3");
       tom_2.play();
       break;
     case "k":
-      var tom_3 = new Audio("./sounds/tom-3.mp3");
+      var tom_3 = new Audio("./tom-3.mp3");
       tom_3.play();
       break;
     case "l":
-      var tom_4 = new Audio("./sounds/tom-4.mp3");
+      var tom_4 = new Audio("./tom-4.mp3");
       tom_4.play();
       break;
     default:
